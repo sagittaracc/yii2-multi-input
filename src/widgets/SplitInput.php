a6a3ef4cfedcf29b445ba59c0d5dd0ba43896d51
arutyunyan <arutyunyan@bolid.ru>
2021-05-18 15:41:06 +0300

    options should not be part of the split input class

--- src/widgets/SplitInput.php
+++ src/widgets/SplitInput.php
@@ -14,39 +14,37 @@ class SplitInput extends ActiveField
 
     public function textSplitInput($options = [])
     {
-        $this->options = array_merge($this->inputOptions, $options);
+        $options = array_merge($this->inputOptions, $options);
         $buttonOptions = isset($options['button']) ? $options['button'] : [];
 
         $this->inputButton = new InputButton($this, $buttonOptions);
 
         $this->parts['{input}'] =
-            $this->splitToInputs() .
+            $this->splitToInputs($options) .
             $this->inputButton->render();
 
         return $this;
     }
 
-    private function splitToInputs()
+    private function splitToInputs($options)
     {
         $inputList = [];
-        $valueList = $this->getValue();
-        $name = isset($this->options['name']) ? $this->options['name'] : "{$this->model->formName()}[{$this->attribute}][]";
+        $separator = ArrayHelper::getValue($this->options, 'separator', ';');
+        $valueList = $this->getValue($separator);
+        $name = isset($options['name']) ? $options['name'] : "{$this->model->formName()}[{$this->attribute}][]";
 
-        Html::addCssClass($this->options, $this->className);
-        ArrayHelper::setValue($this->options, 'data-model', $this->attribute);
+        Html::addCssClass($options, $this->className);
+        ArrayHelper::setValue($options, 'data-model', $this->attribute);
 
         foreach ($valueList as $value) {
-            $inputList[] = Html::input('text', $name, $value, $this->options);
+            $inputList[] = Html::input('text', $name, $value, $options);
         }
 
         return implode('', $inputList);
     }
 
-    private function getValue()
+    private function getValue($separator)
     {
-        $separatorByDefault = ';';
-        $separator = ArrayHelper::getValue($this->options, 'separator', $separatorByDefault);
-
         return is_string($this->model->{$this->attribute}) || is_null($this->model->{$this->attribute})
             ? explode($separator, $this->model->{$this->attribute})
             : $this->model->{$this->attribute};
