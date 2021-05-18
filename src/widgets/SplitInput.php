70d2cd093a3baef367a7190e04b16741d83d202e
arutyunyan <arutyunyan@bolid.ru>
2021-05-18 15:00:47 +0300

    options not defined

--- src/widgets/SplitInput.php
+++ src/widgets/SplitInput.php
@@ -10,33 +10,35 @@ class SplitInput extends ActiveField
 {
     private $className = 'sagittaracc-split-input';
 
+    private $options;
+
     private $inputButton;
 
     public function textSplitInput($options = [])
     {
-        $options = array_merge($this->inputOptions, $options);
+        $this->options = array_merge($this->inputOptions, $options);
         $buttonOptions = isset($options['button']) ? $options['button'] : [];
 
         $this->inputButton = new InputButton($this, $buttonOptions);
 
         $this->parts['{input}'] =
-            $this->splitToInputs($options) .
+            $this->splitToInputs() .
             $this->inputButton->render();
 
         return $this;
     }
 
-    private function splitToInputs($options)
+    private function splitToInputs()
     {
         $inputList = [];
         $valueList = $this->getValue();
-        $name = isset($options['name']) ? $options['name'] : "{$this->model->formName()}[{$this->attribute}][]";
+        $name = isset($this->options['name']) ? $this->options['name'] : "{$this->model->formName()}[{$this->attribute}][]";
 
-        Html::addCssClass($options, $this->className);
-        ArrayHelper::setValue($options, 'data-model', $this->attribute);
+        Html::addCssClass($this->options, $this->className);
+        ArrayHelper::setValue($this->options, 'data-model', $this->attribute);
 
         foreach ($valueList as $value) {
-            $inputList[] = Html::input('text', $name, $value, $options);
+            $inputList[] = Html::input('text', $name, $value, $this->options);
         }
 
         return implode('', $inputList);
@@ -45,7 +47,7 @@ class SplitInput extends ActiveField
     private function getValue()
     {
         $separatorByDefault = ';';
-        $separator = ArrayHelper::getValue($options, 'separator', $separatorByDefault);
+        $separator = ArrayHelper::getValue($this->options, 'separator', $separatorByDefault);
 
         return is_string($this->model->{$this->attribute}) || is_null($this->model->{$this->attribute})
             ? explode($separator, $this->model->{$this->attribute})
